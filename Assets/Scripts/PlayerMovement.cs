88a2f65f73a13c55b73fe59256c6a3ebfce9f751
ProfL3501 <ProfL3501@lab.aca>
2023-10-18 17:57:55 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float MovementSpeed = 3;
+
+    private Vector3 direction = Vector3.zero;
+
+    private CharacterController characterController;
+
+    private void Awake() 
+    {
+        characterController = GetComponent<CharacterController>();   
+    }
+
+    private void Update() 
+    {
+        characterController.Move(
+            transform.forward * direction.normalized.z * Time.deltaTime * MovementSpeed
+            + transform.right * direction.normalized.x * Time.deltaTime * MovementSpeed
+        );
+
+        Debug.Log(transform.forward);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        var data = value.Get<Vector2>();
+        direction = new Vector3(
+            data.x,
+            0f,
+            data.y
+        );
+    }
+
+    private void OnFire(InputValue value)
+    {
+
+    }
+
+    private void OnLook(InputValue value)
+    {
+
+    }
+}
